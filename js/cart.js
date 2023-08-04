dcd8f1935b02f866638d7da041fe40b7861a7860
Joaquin Gonzalez  Menza <holaj3415@gmail.com>
2023-08-03 23:29:47 -0300

    PreEntrega3-de-Gonzalez

---
+++ js/cart.js
@@ -0,0 +1,55 @@
+let carrito = []
+
+const productoContenedor = document.querySelector(".todos-productos");
+
+productoContenedor.addEventListener('click', (e) => {
+  if (e.target.classList.contains('agregar')) {
+    validarProductoEnCarrito(e.target.id);
+  }
+});
+
+const validarProductoEnCarrito = (id) => {
+  const estaRepetido = carrito.some(producto => producto.id == id);
+  
+  if (!estaRepetido) {
+    const producto = productos.find(producto => producto.id == id);
+    carrito.push(producto);
+    pintarProductoCarrito(producto);
+  } else {
+    const producto = carrito.find(producto => producto.id == id);
+    const cantidad = document.getElementById(`cantidad${producto.id}`);
+    producto.cantidad++;
+    cantidad.innerText = `Cantidad: ${producto.cantidad}`;
+  }
+
+  saveProductStorage(producto);
+};
+
+const pintarProductoCarrito = (producto) => {
+  const contenedor = document.getElementById('carrito-contenedor');
+  const div = document.createElement('div');
+  div.classList.add('productoEnCarrito');
+
+  div.innerHTML = `
+    <p>${producto.nombre}</p>
+    <p>$ ${producto.precio}</p>
+    <p id=cantidad${producto.id}>Cantidad: ${producto.cantidad}</p>
+    <button class="btn waves-effect waves-ligth boton-eliminar" value="${producto.id}">X</button>
+  `;
+
+  contenedor.appendChild(div);
+
+  const botonEliminar = div.querySelector('.boton-eliminar');
+  botonEliminar.addEventListener('click', () => {
+    eliminarProductoDelCarrito(producto.id);
+    div.remove();
+  });
+
+  contenedor.appendChild(div);
+};
+
+const eliminarProductoDelCarrito = (id) => {
+  carrito = carrito.filter(producto => producto.id !== id);
+};
+
+console.log(carrito);
