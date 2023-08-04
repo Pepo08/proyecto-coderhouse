23f8b5d903497d55a2ea455c53ae9bfcfeb432ed
Joaquin Gonzalez  Menza <holaj3415@gmail.com>
2023-08-03 23:46:56 -0300

    PreEntrega3-de-GonzalezDiego

--- js/cart.js
+++ js/cart.js
@@ -52,4 +52,38 @@ const eliminarProductoDelCarrito = (id) => {
   carrito = carrito.filter(producto => producto.id !== id);
 };
 
+const pintarCarrito = (carrito) => {
+  const contenedor = document.getElementById('carrito-contenedor')
+
+  contenedor.innerHTML = ''
+
+  carrito.forEach(producto => {
+    const div = document.createElement('div')
+    div.classList.add('productoEnCarrito')
+
+    div.innerHTML = `
+      <p>${producto.nombre}</p>
+      <p>$ ${producto.precio}</p>
+      <p id=cantidad${producto.id}>Cantidad: ${producto.cantidad}</p>
+      <button class="btn waves-effect waves-ligth boton-eliminar" value="${producto.id}">X</button>
+    `
+    contenedor.appendChild(div)
+  });
+}
+
+const actualizarTotalesCarrito = (carrito) => {
+  const totalCantidad = carrito.reduce((acc, item) => acc + item.cantidad, 0)
+  const totalCompra = carrito.reduce((acc, item) => acc + (item.precio * item.cantidad), 0)
+
+  pintarTotalesCarrito(totalCantidad, totalCompra)
+  guardarCarritoStorage(carrito)
+}
+
+const pintarTotalesCarrito = (totalCantidad, totalCompra) => {
+  const contadorCarrito = document.getElementById('contador-carrito')
+  const precioTotal = document.getElementById('precio-total')
+
+  contadorCarrito.innerText = totalCantidad
+  precioTotal.innerText = totalCompra
+}
 console.log(carrito);
--- js/storage.json.js
+++ js/storage.json.js
@@ -1,29 +1,16 @@
-const saveProductStorage = (carrito) => {
-    sessionStorage.setItem('producto', JSON.stringify(carrito));
-};
+const guardarCarritoStorage = (carrito) => {
+    localStorage.setItem('carrito', JSON.stringify(carrito))
+}
 
-const getProductStorage = () => {
-    const carritoStorage = JSON.parse(sessionStorage.getItem('producto'));
-    return carritoStorage || [];
-};
+const obtenerCarritoStorage = () => {
+    const carritoStorage = JSON.parse(localStorage.getItem('carrito'))
+    return carritoStorage
+}
 
-const getProduct = () => {
-    carrito = getProductStorage();
-    const contenedor = document.getElementById('carrito-contenedor');
-    contenedor.innerHTML = '';
-    carrito.forEach(producto => {
-        const div = document.createElement('div');
-        div.classList.add('productoEnCarrito');
-        div.innerHTML = `
-    <p>${producto.nombre}</p>
-    <p>$ ${producto.precio}</p>
-    <p id=cantidad${producto.id}>Cantidad: ${producto.cantidad}</p>
-    <button class="btn waves-effect waves-ligth boton-eliminar" value="${producto.id}">X</button>
-    `;
-        contenedor.appendChild(div);
-    });
-};
-
-document.addEventListener("DOMContentLoaded", () => {
-    getProduct();
-});
+const cargarCarrito = () => {
+    if (localStorage.getItem('carrito')) {
+        carrito = obtenerCarritoStorage()
+        pintarCarrito(carrito)
+        actualizarTotalesCarrito(carrito)
+    }
+}
